d0813731b0103e69dcaebf04b03aa2e5a2394c6b
Amit Giri <thisisag840@gmail.com>
2023-02-14 16:47:18 +0530

    physics pdfs added

--- src/components/physics/Physics.jsx
+++ src/components/physics/Physics.jsx
@@ -26,52 +26,52 @@ const Physics = () => {
     <Tbody>
       <Tr>
         <Td>1</Td>
-        <Td>Electric Charges and Fields</Td>
+        <Td><a href='https://drive.google.com/file/d/1Vx5nfXygnVJqkJvmOIf3keRJRmVy_rKB/view?usp=sharing' target={'_blank'}>Electric Charges and Fields</a></Td>
         <Td>Electrostatics</Td>
       </Tr>
       <Tr>
         <Td>2</Td>
-        <Td>Electrostatic Potential and Capacitance</Td>
+        <Td><a href='https://drive.google.com/file/d/1U-ZEIGgc6b32w8YwzOVCKj0dqpSYbMo_/view?usp=sharing' target={'_blank'}>Electrostatic Potential and Capacitance</a></Td>
         <Td>Electrostatics</Td>
       </Tr>
       <Tr>
         <Td>3</Td>
-        <Td> Current Electricity</Td>
+        <Td><a href='https://drive.google.com/file/d/1U-ZEIGgc6b32w8YwzOVCKj0dqpSYbMo_/view?usp=sharing' target={'_blank'}> Current Electricity</a></Td>
         <Td> Current Electricity</Td>
       </Tr>
       <Tr>
         <Td>4</Td>
-        <Td> Moving Charges and Magnetism</Td>
+        <Td><a href='https://drive.google.com/file/d/1Z9NIO7PNaKizQkfc9CBKI3mQ4oXgLEhJ/view?usp=share_link' target={'_blank'}> Moving Charges and Magnetism</a></Td>
         <Td> Magnetic Effects of Current and Magnetism</Td>
       </Tr>
       <Tr>
         <Td>5</Td>
-        <Td> Magnetism and Matter</Td>
+        <Td><a href='https://drive.google.com/file/d/1l7DELtc37RBGlCB1-sCz97kMH7rwkTUh/view?usp=share_link' target={'_blank'}> Magnetism and Matter</a></Td>
         <Td> Magnetic Effects of Current and Magnetism</Td>
       </Tr>
       <Tr>
         <Td>6</Td>
-        <Td> Electromagnetic Induction</Td>
+        <Td><a href='https://drive.google.com/file/d/1v_nYn8Pr5p1ft5lNvy3ySXetpRUxZ0lY/view?usp=share_link' target={'_blank'}> Electromagnetic Induction</a></Td>
         <Td> Electromagnetic Induction and Alternating Currents</Td>
       </Tr>
       <Tr>
         <Td>7</Td>
-        <Td> Alternating Current</Td>
+        <Td><a href='https://drive.google.com/file/d/1_isVZIBchQe5OMpLhitF6YE4O1Hv6p4f/view?usp=share_link' target={'_blank'}> Alternating Current</a></Td>
         <Td> Electromagnetic Induction and Alternating Currents</Td>
       </Tr>
       <Tr>
         <Td>8</Td>
-        <Td> Ray Optics and Optical Instruments</Td>
+        <Td><a href='https://drive.google.com/file/d/1r4z9i4hcJpDWzkJC-NHzCCnjZSf1-DEC/view?usp=share_link' target={'_blank'}> Ray Optics and Optical Instruments</a></Td>
         <Td> Electromagnetic Waves</Td>
       </Tr>
       <Tr>
         <Td>9</Td>
-        <Td> Current Electricity</Td>
+        <Td><a href='https://drive.google.com/file/d/1YDv5UhBIO2AIuAy3LhXw05xdN52zkuM1/view?usp=share_link' target={'_blank'}> Current Electricity</a></Td>
         <Td> Optics</Td>
       </Tr>
       <Tr>
         <Td>10</Td>
-        <Td>  Wave Optics</Td>
+        <Td><a href='https://drive.google.com/file/d/1r4z9i4hcJpDWzkJC-NHzCCnjZSf1-DEC/view?usp=share_link' target={'_blank'}>  Wave Optics</a></Td>
         <Td> Optics</Td>
       </Tr>
       <Tr>
@@ -85,17 +85,17 @@ const Physics = () => {
         <Td> Atoms and Nuclei</Td>
       </Tr>
       <Tr>
-        <Td>12</Td>
+        <Td>13</Td>
         <Td> Atoms</Td>
         <Td> Atoms and Nuclei</Td>
       </Tr>
       <Tr>
-        <Td>13</Td>
+        <Td>14</Td>
         <Td> Nuclei</Td>
         <Td> Atoms and Nuclei</Td>
       </Tr>
       <Tr>
-        <Td>12</Td>
+        <Td>15</Td>
         <Td> Semiconductor Electronics: Materials, Devices and Simple Circuits</Td>
         <Td> Electronic Devices</Td>
       </Tr>
